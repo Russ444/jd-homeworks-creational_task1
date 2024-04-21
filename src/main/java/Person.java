e0d11226f779922427f0777d1bf8cc73806744ed
“Ruslan” <borisovr-n@yandex.ru>
2024-04-21 19:13:22 +0300

    Задание выполнено!

---
+++ src/main/java/Person.java
@@ -0,0 +1,76 @@
+import java.util.OptionalInt;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+    protected int age;
+    protected String address;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age, String address) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.address = address;
+    }
+
+
+    public boolean hasAge() {
+        return age > 0;
+    }
+
+    public boolean hasAddress() {
+        return address != null && !address.isEmpty();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public OptionalInt getAge() {
+        return hasAge() ? OptionalInt.of(age) : OptionalInt.empty();
+    }
+
+    public String getAddress() {
+        return address != null ? address : "";
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void happyBirthday() {
+        age++;
+    }
+
+    public PersonBuilder newChildBuilder() {
+        PersonBuilder builder = new PersonBuilder();
+        builder.setSurname(this.surname);
+        builder.setAddress(this.address);
+        return builder;
+    }
+
+
+    @Override
+    public String toString() {
+        return name + " " + surname + ", возраст " + (hasAge() ? age : "неизвестен") + ", проживает в " + (hasAddress() ? address : "неизвестно");
+    }
+
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + surname.hashCode();
+        result = 31 * result + age;
+        result = 31 * result + (address != null ? address.hashCode() : 0);
+        return result;
+    }
+}
